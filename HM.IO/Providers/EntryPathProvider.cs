d6f59a2dcfcb4765306e9b8c362c667b8a58019a
HiroMuraki <419734782@qq.com>
2023-12-31 10:57:01 +0800

    Add test cases for FilesProvider

--- HM.IO/Providers/EntryPathProvider.cs
+++ HM.IO/Providers/EntryPathProvider.cs
@@ -1,9 +1,19 @@
 ﻿namespace HM.IO.Providers;
 
+/// <summary>
+/// Provides an abstract base class for generating entry paths and implements the <see cref="IItemsProvider{T}"/> interface.
+/// </summary>
 public abstract class EntryPathProvider :
     IItemsProvider<EntryPath>
 {
-    public IDirectoryIO DirectoryIO { get; set; } = new DirectoryIO();
+    /// <summary>
+    /// Gets or sets the directory input/output operations for the entry path provider.
+    /// </summary>
+    public IDirectoryIO DirectoryIO { get; protected set; } = new DirectoryIO();
 
+    /// <summary>
+    /// Enumerates and returns a collection of entry paths.
+    /// </summary>
+    /// <returns>An <see cref="IEnumerable{T}"/> of entry paths.</returns>
     public abstract IEnumerable<EntryPath> EnumerateItems();
 }

9fb6449706ca63b6c3dc806332bd6d98a7997fec
HiroMuraki <419734782@qq.com>
2024-03-16 13:58:58 +0800

    Update Common.ParallelSafe

--- HM.Common/ParallelSafe.cs
+++ HM.Common/ParallelSafe.cs
@@ -2,6 +2,9 @@
 
 public static class ParallelSafe
 {
+    public static void Run(Action action, SemaphoreSlim semaphore)
+        => Run(action, semaphore.Wait, () => semaphore.Release());
+
     public static void Run(Action action, Action waitAction, Action releaseAction)
     {
         waitAction();
@@ -15,6 +18,9 @@ public static class ParallelSafe
             releaseAction();
         }
     }
+
+    public static T Run<T>(Func<T> action, SemaphoreSlim semaphore)
+        => Run(action, semaphore.Wait, () => semaphore.Release());
 
     public static T Run<T>(Func<T> func, Action waitAction, Action releaseAction)
     {
@@ -30,6 +36,9 @@ public static class ParallelSafe
         }
     }
 
+    public static async Task RunAsync(Func<Task> asyncAction, SemaphoreSlim semaphore)
+        => await RunAsync(asyncAction, semaphore.Wait, () => semaphore.Release());
+
     public static async Task RunAsync(Func<Task> asyncAction, Action waitAction, Action releaseAction)
     {
         waitAction();
@@ -43,6 +52,9 @@ public static class ParallelSafe
             releaseAction();
         }
     }
+
+    public static async Task<T> RunAsync<T>(Func<Task<T>> asyncAction, SemaphoreSlim semaphore)
+        => await RunAsync(asyncAction, semaphore.Wait, () => semaphore.Release());
 
     public static async Task<T> RunAsync<T>(Func<Task<T>> asyncAction, Action waitAction, Action releaseAction)
     {

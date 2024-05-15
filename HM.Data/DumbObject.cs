2dd8a627ca08608a4ac8623ce989f93d95824995
HiroMuraki <419734782@qq.com>
2024-05-15 20:25:28 +0800

    Update HM.Data

--- HM.Data/DumbObject.cs
+++ HM.Data/DumbObject.cs
@@ -1,34 +1,37 @@
 ﻿using System.Collections;
+using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 
 namespace HM.Data;
 
-public class DumbObject
+public sealed class DumbObject
 {
     public DumbObject(Object? obj)
     {
-        _obj = obj;
+        _rawObject = obj;
     }
 
-    public Object? RawObject => _obj;
+    public Object? RawObject => _rawObject;
 
-    public Boolean IsNull => _obj is null;
+    public Int32 PropertyCount => AsDictionaryOrNull()?.Count ?? -1;
 
-    public Boolean IsInt32 => _obj is Int32;
+    public Boolean IsNull => _rawObject is null;
 
-    public Boolean IsInt64 => _obj is Int64;
+    public Boolean IsInt32 => _rawObject is Int32;
 
-    public Boolean IsDouble => _obj is Double;
+    public Boolean IsInt64 => _rawObject is Int64;
 
-    public Boolean IsString => _obj is String;
+    public Boolean IsDouble => _rawObject is Double;
 
-    public Boolean IsBoolean => _obj is Boolean;
+    public Boolean IsString => _rawObject is String;
+
+    public Boolean IsBoolean => _rawObject is Boolean;
 
     public Boolean IsCollection
     {
         get
         {
-            if (_obj is null)
+            if (_rawObject is null)
             {
                 return false;
             }
@@ -37,21 +40,19 @@ public class DumbObject
                 return false;
             }
 
-            return _obj.GetType().IsAssignableTo(typeof(ICollection));
+            return _rawObject.GetType().IsAssignableTo(typeof(ICollection));
         }
     }
 
-    public Boolean IsDictionary => _obj is Dictionary<Object, Object>;
-
-    public Int32 PropertyCount => IsDictionary ? -1 : ToDictionary().Keys.Count;
+    public Boolean IsDictionary => _rawObject is IDictionary;
 
     public DumbObject this[String propertyName] => GetProperty(propertyName);
 
     public Boolean HasProperty(String propertyName)
     {
-        if (_obj is Dictionary<Object, Object> dictObj)
+        if (TryToDictionary(out IReadOnlyDictionary<String, DumbObject>? value))
         {
-            return dictObj.ContainsKey(propertyName);
+            return value.ContainsKey(propertyName);
         }
         else
         {
@@ -61,7 +62,7 @@ public class DumbObject
 
     public DumbObject GetProperty(String propertyName)
     {
-        Dictionary<String, DumbObject> dictObj = ToDictionary();
+        IReadOnlyDictionary<String, DumbObject> dictObj = ToDictionary();
 
         if (dictObj.TryGetValue(propertyName, out DumbObject? value))
         {
@@ -75,17 +76,17 @@ public class DumbObject
 
     public T GetProperty<T>(String propertyName)
     {
-        Dictionary<String, DumbObject> dictObj = ToDictionary();
+        IReadOnlyDictionary<String, DumbObject> dictObj = ToDictionary();
 
         if (dictObj.TryGetValue(propertyName, out DumbObject? value))
         {
-            if (value._obj is T result)
+            if (value._rawObject is T result)
             {
                 return result;
             }
             else
             {
-                throw new InvalidCastException($"Unable to cast {value._obj} to {typeof(T)}");
+                throw new InvalidCastException($"Unable to cast {value._rawObject} to {typeof(T)}");
             }
         }
         else
@@ -96,7 +97,7 @@ public class DumbObject
 
     public Boolean TryGetProperty(String propertyName, [NotNullWhen(true)] out DumbObject? value)
     {
-        if (_obj is Dictionary<Object, Object> dictObj && dictObj.TryGetValue(propertyName, out Object? result))
+        if (_rawObject is Dictionary<Object, Object> dictObj && dictObj.TryGetValue(propertyName, out Object? result))
         {
             value = new DumbObject(result);
             return true;
@@ -110,7 +111,7 @@ public class DumbObject
 
     public Boolean TryGetProperty<T>(String propertyName, [NotNullWhen(true)] out T? value)
     {
-        if (_obj is Dictionary<Object, Object> dictObj && dictObj.TryGetValue(propertyName, out Object? result))
+        if (_rawObject is Dictionary<Object, Object> dictObj && dictObj.TryGetValue(propertyName, out Object? result))
         {
             if (result is T t)
             {
@@ -132,14 +133,14 @@ public class DumbObject
 
     public override String? ToString()
     {
-        return _obj?.ToString();
+        return _rawObject?.ToString();
     }
 
     public Int32 ToInt32()
     {
-        ArgumentNullException.ThrowIfNull(_obj);
+        ArgumentNullException.ThrowIfNull(_rawObject);
 
-        return Convert.ToInt32(_obj);
+        return (Int32)_rawObject;
     }
 
     public Boolean TryToInt32(out Int32 value)
@@ -158,9 +159,9 @@ public class DumbObject
 
     public Int64 ToInt64()
     {
-        ArgumentNullException.ThrowIfNull(_obj);
+        ArgumentNullException.ThrowIfNull(_rawObject);
 
-        return Convert.ToInt64(_obj);
+        return (Int64)_rawObject;
     }
 
     public Boolean TryToInt64(out Int64 value)
@@ -179,9 +180,9 @@ public class DumbObject
 
     public Double ToDouble()
     {
-        ArgumentNullException.ThrowIfNull(_obj);
+        ArgumentNullException.ThrowIfNull(_rawObject);
 
-        return Convert.ToDouble(_obj);
+        return (Double)_rawObject;
     }
 
     public Boolean TryToDouble(out Double value)
@@ -200,9 +201,9 @@ public class DumbObject
 
     public Boolean ToBoolean()
     {
-        ArgumentNullException.ThrowIfNull(_obj);
+        ArgumentNullException.ThrowIfNull(_rawObject);
 
-        return Convert.ToBoolean(_obj);
+        return (Boolean)_rawObject;
     }
 
     public Boolean TryToBoolean(out Boolean value)
@@ -219,83 +220,101 @@ public class DumbObject
         }
     }
 
-    public ICollection<DumbObject> ToCollection()
+    public IReadOnlyCollection<DumbObject> ToCollection()
     {
-        if (_obj is IEnumerable<Object> enumerable)
-        {
-            return enumerable.Select(x => new DumbObject(x)).ToArray();
-        }
-        else
-        {
-            throw new InvalidOperationException($"Unable to cast current Object `{_obj}` to {typeof(DumbObject[])}");
-        }
+        return AsCollectionOrNull()?.Select(x => new DumbObject(x)).ToImmutableArray()
+            ?? throw new InvalidOperationException($"Unable to cast current Object `{_rawObject}` to {typeof(DumbObject[])}");
     }
 
-    public ICollection<T> ToCollection<T>()
+    public Boolean TryToCollection([NotNullWhen(true)] out IReadOnlyCollection<DumbObject>? value)
     {
-        if (_obj is IEnumerable<Object> enumerable)
-        {
-            return enumerable.Select(x => (T)x).ToArray();
-        }
-        else
-        {
-            throw new InvalidOperationException($"Unable to cast current Object `{_obj}` to {typeof(DumbObject[])}");
-        }
+        value = AsCollectionOrNull()?.Select(x => new DumbObject(x)).ToImmutableArray();
+        return value is not null;
     }
 
-    public Boolean TryToCollection(out ICollection<DumbObject>? value)
+    public IReadOnlyCollection<T> ToCollection<T>()
     {
-        if (IsCollection)
-        {
-            value = ToCollection();
-            return true;
-        }
-        else
-        {
-            value = null;
-            return false;
-        }
+        return AsCollectionOrNull()?.Select(x => (T)x).ToImmutableArray()
+            ?? throw new InvalidOperationException($"Unable to cast current Object `{_rawObject}` to {typeof(DumbObject[])}");
     }
 
-    public Boolean TryToCollection<T>(out ICollection<T>? value)
+    public Boolean TryToCollection<T>([NotNullWhen(true)] out IReadOnlyCollection<T>? value)
     {
-        if (IsCollection)
-        {
-            value = ToCollection<T>();
-            return true;
-        }
-        else
-        {
-            value = null;
-            return false;
-        }
+        value = AsCollectionOrNull()?.Select(x => (T)x).ToImmutableArray();
+        return value is not null;
     }
 
-    public Dictionary<String, DumbObject> ToDictionary()
+    public IReadOnlyDictionary<String, DumbObject> ToDictionary()
     {
-        if (_obj is not Dictionary<Object, Object> dictObj)
-        {
-            throw new InvalidOperationException($"Unable to cast current Object `{_obj}` to {typeof(Dictionary<,>)}");
-        }
+        IReadOnlyDictionary<Object, Object>? dictObj = AsDictionaryOrNull()
+             ?? throw new InvalidOperationException($"Unable to cast current Object `{_rawObject}` to {typeof(Dictionary<,>)}");
 
-        return dictObj.ToDictionary(
+        return dictObj.ToImmutableDictionary(
             k => k.Key.ToString() ?? throw new ArgumentNullException(),
             v => new DumbObject(v.Value));
     }
 
-    public Dictionary<String, T> ToDictionary<T>()
+    public Boolean TryToDictionary([NotNullWhen(true)] out IReadOnlyDictionary<String, DumbObject>? value)
     {
-        if (_obj is not Dictionary<Object, Object> dictObj)
+        IReadOnlyDictionary<Object, Object>? dictObj = AsDictionaryOrNull();
+        if (dictObj is null)
         {
-            throw new InvalidOperationException($"Unable to cast current Object `{_obj}` to {typeof(Dictionary<,>)}");
+            value = null;
+            return false;
         }
 
-        return dictObj.ToDictionary(
+        value = dictObj.ToImmutableDictionary(
+            k => k.Key.ToString()!,
+            v => new DumbObject(v.Value));
+        return true;
+    }
+
+    public IReadOnlyDictionary<String, T> ToDictionary<T>()
+    {
+        IReadOnlyDictionary<Object, Object>? dictObj = AsDictionaryOrNull()
+             ?? throw new InvalidOperationException($"Unable to cast current Object `{_rawObject}` to {typeof(Dictionary<,>)}");
+
+        return dictObj.ToImmutableDictionary(
             k => k.Key.ToString() ?? throw new ArgumentNullException(),
             v => (T)v.Value);
     }
 
+    public Boolean TryToDictionary<T>([NotNullWhen(true)] out IReadOnlyDictionary<String, T>? value)
+    {
+        IReadOnlyDictionary<Object, Object>? dictObj = AsDictionaryOrNull();
+        if (dictObj is null)
+        {
+            value = null;
+            return false;
+        }
+
+        var result = new Dictionary<String, T>();
+        foreach ((Object k, Object v) in dictObj)
+        {
+            String? keyString = k?.ToString();
+            if (keyString is null || v is not T valueT)
+            {
+                value = default;
+                return false;
+            }
+            else
+            {
+                result[keyString] = valueT;
+            }
+        }
+        value = result.ToImmutableDictionary();
+        return true;
+    }
+
     #region NonPublic
-    private readonly Object? _obj;
+    private readonly Object? _rawObject;
+    private IReadOnlyDictionary<Object, Object>? AsDictionaryOrNull()
+    {
+        return _rawObject as IReadOnlyDictionary<Object, Object>;
+    }
+    private IReadOnlyCollection<Object>? AsCollectionOrNull()
+    {
+        return _rawObject as IReadOnlyCollection<Object>;
+    }
     #endregion
 }
